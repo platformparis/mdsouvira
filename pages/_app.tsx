5b661eafcbbd31da46bb3751cffeb41ab4753a9b
llindemann <2153696+llindemann@users.noreply.github.com>
2024-03-01 20:14:12 +0100

    prevent dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import Head from "next/head";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
+      <Head>
+        <meta name="color-scheme" content="light only"></meta>
+      </Head>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
