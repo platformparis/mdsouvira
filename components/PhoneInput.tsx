9c6aa7f3fff72e839ee9add28d6beaec9c882db1
llindemann <2153696+llindemann@users.noreply.github.com>
2024-03-01 21:56:15 +0100

    use FR locale for PhoneInput

--- components/PhoneInput.tsx
+++ components/PhoneInput.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import { E164Number } from 'libphonenumber-js';
 import 'react-phone-number-input/style.css'
+import fr from 'react-phone-number-input/locale/fr'
 import SrcPhoneInput, { isPossiblePhoneNumber } from 'react-phone-number-input';
 
 interface PhoneInputProps {
@@ -14,6 +15,7 @@ export function PhoneInput({ className, placeholder, name }: PhoneInputProps) {
       let [value, setValue]: [E164Number | undefined, any] = useState()
       return <SrcPhoneInput
                   onChange={setValue}
+                  labels={fr}
                   value={value}
                   phonevalue={value}
                   name={name}
